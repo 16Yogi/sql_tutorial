f3cabfae30c0b1961aa5fca9b0b91d4b836f844a
Yogendra Meravi <85734470+16Yogi@users.noreply.github.com>
2024-11-30 18:59:41 +0530

    commit
    commit

---
+++ order_by.sql
@@ -0,0 +1,14 @@
+SELECT * FROM  table2 ORDER BY price;
+
+-- assending order
+SELECT id,name,age FROM table1 ORDER BY age ASC;
+
+
+--- dessending order
+-- SELECT id,name,age FROM table2 ORDER BY age DESC;
+
+SELECT * FROM table1 ORDER BY name;
+
+SELECT * FROM table1 ORDER BY name,age;
+
+SELECT * FROM table1 ORDER BY name ASC, age DESC;
