6e3395ce340602944476e56530bd4daf0464767c
Yogendra Meravi <85734470+16Yogi@users.noreply.github.com>
2025-01-01 10:12:09 +0530

    commit
    commit

---
+++ insert.sql
@@ -0,0 +1,9 @@
+-- INSERT INTO table2(job_role,location,salary)
+-- VALUES
+-- ('dev','bpl','250000');
+
+
+INSERT INTO table2(job_role,location,salary)
+VALUES
+('dev','hyderabad','200000'),
+('dev','chennai','300000');
